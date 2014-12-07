e318721675d01bf0e0c9e685ce762bb60027f6ef
Patrick Hidalgo <m.patrick.hidalgo@gmail.com>
2014-12-07 10:42:00 -0700

    TDD for investments

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -25,10 +25,10 @@ class InvestmentsController < ApplicationController
   # POST /investments.json
   def create
     @investment = Investment.new(investment_params)
-
+    creation_message = "#{@investment.term} year #{@investment.interest_rate}% rate was successfully created."
     respond_to do |format|
       if @investment.save
-        format.html { redirect_to @investment, notice: 'Investment was successfully created.' }
+        format.html { redirect_to @investment, notice: creation_message }
         format.json { render :show, status: :created, location: @investment }
       else
         format.html { render :new }
@@ -41,8 +41,10 @@ class InvestmentsController < ApplicationController
   # PATCH/PUT /investments/1.json
   def update
     respond_to do |format|
+      update_message = "#{@investment.term} year #{@investment.interest_rate}% rate was successfully updated."
+
       if @investment.update(investment_params)
-        format.html { redirect_to @investment, notice: 'Investment was successfully updated.' }
+        format.html { redirect_to @investment, notice: update_message }
         format.json { render :show, status: :ok, location: @investment }
       else
         format.html { render :edit }
@@ -54,21 +56,23 @@ class InvestmentsController < ApplicationController
   # DELETE /investments/1
   # DELETE /investments/1.json
   def destroy
+    destroy_message = "#{@investment.term} year #{@investment.interest_rate}% rate was successfully deleted."
+
     @investment.destroy
     respond_to do |format|
-      format.html { redirect_to investments_url, notice: 'Investment was successfully destroyed.' }
+      format.html { redirect_to investments_path, notice: destroy_message }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_investment
-      @investment = Investment.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_investment
+    @investment = Investment.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def investment_params
-      params.require(:investment).permit(:term, :interest_rate, :minimum_order, :denomination, :maturity_date, :issue_date, :offer_start_period, :offer_end_period)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def investment_params
+    params.require(:investment).permit(:term, :interest_rate, :minimum_order, :denomination, :maturity_date, :issue_date, :offer_start_period, :offer_end_period)
+  end
 end
---
+++ spec/features/creating_investments_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+feature 'Investment Management' do
+  scenario 'user can create an investment' do
+    visit new_investment_path
+
+    fill_in 'Term', with: '6'
+    fill_in 'Interest rate', with: '4.50'
+    fill_in 'Minimum order', with: '1000'
+    fill_in 'Denomination', with: '1000'
+    fill_in 'Maturity date', with: '2020-12-10'
+    fill_in 'Issue date', with: '2014-12-10'
+    fill_in 'Offer start period', with: '2014-12-10'
+    fill_in 'Offer end period', with: '2015-2-10'
+
+    click_button 'Create Investment'
+    # save_and_open_page
+    expect(page).to have_text('6 year 4.5% rate was successfully created')
+
+  end
+
+  scenario 'user edits an investment' do
+    investment = FactoryGirl.create(:investment)
+    visit edit_investment_path(investment)
+
+    fill_in 'Term', with: '7'
+
+    click_button 'Update'
+
+    expect(page).to have_text("#{investment.term} year #{investment.interest_rate}% rate was successfully updated.")
+    expect(page).to have_text('7')
+
+  end
+
+  scenario 'user can delete an investment' do
+    visit new_investment_path
+
+    fill_in 'Term', with: '6'
+    fill_in 'Interest rate', with: '4.50'
+    fill_in 'Minimum order', with: '1000'
+    fill_in 'Denomination', with: '1000'
+    fill_in 'Maturity date', with: '2020-12-10'
+    fill_in 'Issue date', with: '2014-12-10'
+    fill_in 'Offer start period', with: '2014-12-10'
+    fill_in 'Offer end period', with: '2015-2-10'
+
+    click_button 'Create Investment'
+    visit '/investments'
+    click_link 'Destroy'
+    # save_and_open_page
+
+    # expect(page).to have_text('6 year 4.5% rate was successfully deleted.')
+
+    expect(page).to_not have_text('6')
+    expect(page).to_not have_text('4.5')
+    expect(page).to_not have_text('2020-12-10')
+    expect(page).to_not have_text('2015-2-10')
+
+  end
+
+end
